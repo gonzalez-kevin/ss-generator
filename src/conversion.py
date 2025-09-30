4a0347fa9dd17802ba2d952267c56436842543ac
Kevin Gonzalez <137135509+gonzalez-kevin@users.noreply.github.com>
2025-09-29 19:23:07 -0700

    convert text to nodes

--- src/conversion.py
+++ src/conversion.py
@@ -1,5 +1,7 @@
 from textnode import TextNode, TextType
 from htmlnode import LeafNode
+from extract import extract_markdown_images, extract_markdown_links
+import re
 
 def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.TEXT:
@@ -53,3 +55,87 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 new_nodes.append(TextNode(part, text_type))
 
     return new_nodes
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        text = node.text
+        matches = list(re.finditer(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text))
+
+        if not matches:
+            new_nodes.append(node)
+            continue
+
+        last_index = 0
+        for match in matches:
+            start, end = match.span()
+            alt_text, url = match.groups()
+
+            # text before the image
+            if start > last_index:
+                new_nodes.append(TextNode(text[last_index:start], TextType.TEXT))
+
+            # image node
+            new_nodes.append(TextNode(alt_text, TextType.IMAGE, url))
+
+            last_index = end
+
+        # any remaining text
+        if last_index < len(text):
+            new_nodes.append(TextNode(text[last_index:], TextType.TEXT))
+
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        text = node.text
+        matches = list(re.finditer(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text))
+
+        if not matches:
+            new_nodes.append(node)
+            continue
+
+        last_index = 0
+        for match in matches:
+            start, end = match.span()
+            anchor_text, url = match.groups()
+
+            # text before the link
+            if start > last_index:
+                new_nodes.append(TextNode(text[last_index:start], TextType.TEXT))
+
+            # link node
+            new_nodes.append(TextNode(anchor_text, TextType.LINK, url))
+
+            last_index = end
+
+        # any remaining text
+        if last_index < len(text):
+            new_nodes.append(TextNode(text[last_index:], TextType.TEXT))
+
+    return new_nodes
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    
+    return nodes
+    
